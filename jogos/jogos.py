fe9a114c541ede68526e0d55fc1fa6c91582d2ff
Marcelo Costa <mbpcosta01@gmail.com>
2021-09-16 14:51:15 -0300

    .

--- jogos/jogos.py
+++ jogos/jogos.py
@@ -1,18 +1,22 @@
 import forca
 import adivinhacao
 
+def escolhe_jogo():
+    print("********************************")
+    print("*******Escolha o seu jogo*******")
+    print("********************************")
 
-print("********************************")
-print("*******Escolha o seu jogo*******")
-print("********************************")
+    print("(1) Forca (2) Adivinhação")
 
-print("(1) Forca (2) Adivinhação")
+    jogo = int(input("Qual jogo? "))
 
-jogo = int(input("Qual jogo? "))
+    if(jogo == 1):
+        print("Jogando Forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
 
-if(jogo == 1):
-    print("Jogando Forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("Jogando adivinhação")
-    adivinhacao.jogar()
+
+if(__name__== "__main__"):
+    escolhe_jogo()
